a99d56704ccb33f36e923e78ce1ee28cb7b96552
Web Development <info@toniramon.es>
2020-08-06 16:45:28 +0200

    adding node-sass and homepage component with basic styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from "./homepage.component";
+import './homepage.styles.scss';
+
 function App() {
   return (
     <div>
-      Hello World
+      <HomePage />
     </div>
   );
 }
