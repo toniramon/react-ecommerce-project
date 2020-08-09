659f50baa9c42f180e27121cd402e901fc6b2b9c
Web Development <info@toniramon.es>
2020-08-09 18:08:38 +0200

    update project with collection item and style the component.

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './collection-preview.styles.scss';
+import CollectionItem from "../collection-item/collection-item.component";
 
 const CollectionPreview = ({ title, items}) => (
     <div className='collection-preview'>
@@ -9,8 +10,8 @@ const CollectionPreview = ({ title, items}) => (
             {
                 items
                     .filter((item, idx) => idx < 4)
-                    .map(item => (
-                    <div key={item.id}>{ item.name }</div>
+                    .map(({ id, ...otherItemsProps}) => (
+                    <CollectionItem key={ id } {...otherItemsProps} />
                 ))
             }
         </div>
