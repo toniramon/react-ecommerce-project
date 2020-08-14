2cfaa3bab559242158199981f053315b5ef3b3e2
Web Development <info@toniramon.es>
2020-08-14 12:52:25 +0200

    added redux store and user actions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import { BrowserRouter} from "react-router-dom";
 import { Provider } from 'react-redux';
 
+// Import redux store
+import store from './redux/store';
+
+import App from './App';
+import './index.css';
+
+
 ReactDOM.render(
-    <Provider>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
